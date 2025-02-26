95cf81514b017f9c5c4381ec9b8b2ae6fdb21289
ArcherHeffern <heffernarcher@gmail.com>
2025-02-26 12:05:07 -0500

    fizz buzz

--- 3_intro_to_c/09_fizz_buzz_function.c
+++ 3_intro_to_c/09_fizz_buzz_function.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+
+void fizz_buzz(int n);
+
 int main() {
-    for (int i = 1; i <= 100; i++) {
+	fizz_buzz(100);
+}
+
+void fizz_buzz(int n) {
+    for (int i = 1; i <= n; i++) {
         if (i % 3 == 0 && i % 5 == 0) {
             printf("FizzBuzz\n");
         } else if (i % 3 == 0) {
