110b7eb20a3356607f7e1441b397e17fb385ca7e
ArcherHeffern <heffernarcher@gmail.com>
2025-04-08 15:16:40 -0400

    malloc for structs

--- 8_structs_and_data_structures/01_struct.c
+++ 8_structs_and_data_structures/01_struct.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Point {
 	int x;
@@ -10,5 +11,13 @@ int main() {
 	p1.x = 5;
 	p1.y = 4;
 
-	struct Point p2 = { .x = 1, .y = 2 };
+	struct Point p2 = { 
+		.x = 1, 
+		.y = 2 
+	};
+
+	// Heap allocated points
+	struct Point *p3 = malloc(sizeof(struct Point));
+	(*p3).x = 1;
+	p3->y = 3; // Shorthand for above
 }
